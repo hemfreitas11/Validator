3fadf993504cf5eb55c80591469f1593d7787646
hemfreitas11 <hemfreitas11@users.noreply.github.com>
2021-03-24 12:21:01 -0300

    Start

---
+++ src/main/java/me/bkrmt/bkcore/validator/Validator.java
@@ -0,0 +1,17 @@
+package me.bkrmt.bkcore.validator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Validator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
